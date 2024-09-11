778563dc94c50fce6e9914a32c15830e4f945aa0
unknown <elmarchik2007@gmail.com git config --global user.name elmargit config --global user.email elmarchik2007@gmail.com$  git config --global user.email elmarchik2007@gmail.com>
2024-09-11 08:41:21 +0300

    add new commit

---
+++ mun.js
@@ -0,0 +1,19 @@
+// let myArray = ["munira","meerim","isa","aziz"];
+// let a = myArray[2];
+// console.log(a);
+
+
+// let list = ["1","2","3","4"];
+// let.push(2);
+// console.log(list);
+
+
+
+// let list =["2","8","9","7"];
+// list.pop();
+// console.log(list);
+
+function myfunction(){
+let text ="hello "
+ console.log(text)
+}
